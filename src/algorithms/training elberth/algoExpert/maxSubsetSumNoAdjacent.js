d7131fb30dc351e34be5f084e8da8476af64751b
DataPerro <elberthcabrales@gmail.com>
2022-09-03 16:52:58 -0600

    test

---
+++ src/algorithms/training elberth/algoExpert/maxSubsetSumNoAdjacent.js
@@ -0,0 +1,15 @@
+function maxSubsetSumNoAdjacent(array) {
+  if (!array.length) return 0;
+  if (array.length === 1) return array[0];
+  const maxSum = array.slice();
+  maxSum[1] = Math.max(array[0], array[1]);
+  for (let i = 2; i < array.length; i++) {
+    maxSum[i] = Math.max(array[i - 1], array[i - 2] + array[i]);
+  }
+  console.log(maxSum);
+  return maxSum[maxSum.length - 1];
+}
+
+const result = maxSubsetSumNoAdjacent([30, 25, 50, 55, 100, 120]); // expected 205
+
+console.log(result);
